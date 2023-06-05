b5cbf7dbeea3ed1c0d30a229e4cbe963b4928f5d
StrandGear <strandgear@mail.ru>
2023-06-05 17:21:53 +0200

    Camera scale added

--- Assets/Scripts/CameraRotate.cs
+++ Assets/Scripts/CameraRotate.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraRotate : MonoBehaviour
 {
-    public float Sensitivity;
-
-    float _targetRotation, 
+    public float Sensitivity, MaxVerticalRotation = 60f, MinCamScale, MaxCamScale;
+    public CinemachineVirtualCamera Cam;
+    float _targetRotation,
+          _lenseDistanceMultiplier,
           _btnPressed = 0;
+
+    Ray ray;
+    RaycastHit hit;
 
     void Update()
     {
+        #region If right mouse btn down 
         if (Input.GetMouseButtonDown(1))
         {
             _btnPressed = 1;
@@ -19,10 +25,25 @@ public class CameraRotate : MonoBehaviour
         {
             _btnPressed = 0;
         }
+        #endregion
 
+        #region Camera rotation horizontally
         _targetRotation += Input.GetAxis("Mouse X") * Sensitivity * _btnPressed;
         _targetRotation += Input.GetAxis("RightJoystick_X") * Sensitivity;
-
         transform.localEulerAngles = new Vector3(0, _targetRotation, 0);
+        #endregion
+
+        #region Scale up/down when scrolling a mousewheel
+        if (Input.mouseScrollDelta.y > 0)
+            _lenseDistanceMultiplier = -100f;
+        else if (Input.mouseScrollDelta.y < 0)
+            _lenseDistanceMultiplier = 100f;
+        else
+            _lenseDistanceMultiplier = 0;
+
+        Cam.m_Lens.FieldOfView += _lenseDistanceMultiplier * Sensitivity * Time.deltaTime;
+        Cam.m_Lens.FieldOfView = Mathf.Clamp(Cam.m_Lens.FieldOfView, MinCamScale, MaxCamScale);
+        print(Cam.m_Lens.FieldOfView);
+        #endregion
     }
 }
