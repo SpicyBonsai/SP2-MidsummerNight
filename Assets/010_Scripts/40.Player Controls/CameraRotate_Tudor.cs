fb30a79c39df94ea71b178b139938b573bb72333
saveciprian <60888561+saveciprian@users.noreply.github.com>
2023-07-08 19:28:27 +0200

    removed unnecessary comments

--- Assets/010_Scripts/40.Player Controls/CameraRotate_Tudor.cs
+++ Assets/010_Scripts/40.Player Controls/CameraRotate_Tudor.cs
@@ -1,84 +1,65 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraRotate_Tudor : MonoBehaviour
 {
-    public float Sensitivity;
+    [SerializeField] private float desiredTime;
+    [SerializeField] private float returnToFacingDelay;
+    public float sensitivity;
     public Transform playerTransform;
-    float _targetRotation;
-    [SerializeField] float desiredTime;
-    [SerializeField] float returnToFacingDelay;
-    float lerpDuration;
-    Coroutine returnToFacing = null;
-    float elapsedTime = 0;
+    private float _targetRotation;
+    private float _lerpDuration;
+    private Coroutine _returnToFacing;
+    private float _elapsedTime;
 
-    private void Start() {
+    private void Start() 
+    {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
     }
-    void Update()
+    
+    private void Update()
     {
         if (InputManager.GetInstance().RightClick)
         {
-            //_targetRotation = transform.forward.y;
-            if(returnToFacing != null)
+            if(_returnToFacing != null)
             {
-                StopCoroutine(returnToFacing);
-                returnToFacing = null;
+                StopCoroutine(_returnToFacing);
+                _returnToFacing = null;
             }
-
-            // transform.rotation = Quaternion.Euler(0.0f, playerTransform.rotation.y * -0.1f, 0.0f);
-            // _targetRotation = transform.rotation.y;
-
-
-            //Cursor.lockState = CursorLockMode.Locked;
-            _targetRotation += InputManager.GetInstance().MouseMovement.x * Sensitivity;
-                // _targetRotation += Input.GetAxis("Mouse X") * Sensitivity;
-                // _targetRotation += Input.GetAxis("RightJoystick_X") * Sensitivity;
-
+            
+            _targetRotation += InputManager.GetInstance().MouseMovement.x * sensitivity;
             transform.localRotation = Quaternion.Euler(0f, _targetRotation, 0f);
-            //transform.localEulerAngles = new Vector3(0, _targetRotation, 0);
         }
-        else if(InputManager.GetInstance().LeftClick){
-            //_targetRotation = transform.localRotation.y;
-            if(Vector3.Dot(transform.forward, playerTransform.forward) < 0.99f && returnToFacing == null)
+        else if(InputManager.GetInstance().LeftClick)
+        {
+            if(Vector3.Dot(transform.forward, playerTransform.forward) < 0.99f && _returnToFacing == null)
             {
-                
-                returnToFacing = StartCoroutine(ReturnToFacing());
+                _returnToFacing = StartCoroutine(ReturnToFacing());
             }
-
-
-            //Cursor.lockState = CursorLockMode.None; 
         }
 
-        if(returnToFacing != null)
+        if(_returnToFacing != null)
         {
             _targetRotation = transform.localRotation.eulerAngles.y;
-            // Debug.Log("Rotating");
         }
     }
 
-    IEnumerator ReturnToFacing()
+    private IEnumerator ReturnToFacing()
     {
-        elapsedTime = 0;
-        lerpDuration = desiredTime;
-        Vector3 initialDirection = transform.forward;
+        _elapsedTime = 0;
+        _lerpDuration = desiredTime;
+        var initialDirection = transform.forward;
         yield return new WaitForSeconds(returnToFacingDelay);
 
-        while(lerpDuration < 1)
+        while(_lerpDuration < 1)
         {
-            elapsedTime += Time.deltaTime;
-            lerpDuration = elapsedTime/desiredTime;
-            transform.forward = Vector3.Lerp(initialDirection, playerTransform.forward, lerpDuration);
+            _elapsedTime += Time.deltaTime;
+            _lerpDuration = _elapsedTime/desiredTime;
+            transform.forward = Vector3.Lerp(initialDirection, playerTransform.forward, _lerpDuration);
             
             yield return new WaitForEndOfFrame();
         }
 
-        returnToFacing = null;
-    }
-
-    private float LerpFloat(float from, float to, float inTime)
-    {
-        return (from * (1f - inTime)) + (to * inTime);
+        _returnToFacing = null;
     }
 }
